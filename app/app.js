09ff57eed40e6f3298ae0506efccce494d4051f7
luisma0330 <luisma0330@gmail.com>
2022-05-07 21:19:27 -0500

    6. PUT Crea un endpoint que se encargue de actualizar un explorer

--- app/app.js
+++ app/app.js
@@ -31,6 +31,13 @@ app.post('/v1/explorers', (req, res) => {
     res.status(201).json({message: "Created"})
 })
 
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`API Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body //Parámetros de un cliente
+    res.status(200).json({message: "Updated"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
